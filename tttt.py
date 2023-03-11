a585577e737d740b7a30c681ae29c2ac0263b030
KingHaHaHai <125114735+KingHaHaHai@users.noreply.github.com>
2023-03-11 11:52:03 +0800

    Add files via upload

--- tttt.py
+++ tttt.py
@@ -7,8 +7,10 @@ I2C = machine.I2C(sda=machine.Pin(21),scl=machine.Pin(22))
 m水位 = machine.ADC(machine.Pin(34))
 m温湿 = dht.DHT11(machine.Pin(2))
 mCO2 = machine.ADC(machine.Pin(35))
+m显示屏 = ssd1306.SSD1306_I2C(128, 64, I2C)
 
 
 while 1:
-    m温湿.measure()
-    print(m温湿)
+    pass
+    # m温湿.measure()
+    # print(m温湿)
