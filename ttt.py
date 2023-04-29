e776e389b692ddf62f49cdb33399d53ef3bd74cc
KingHaHaHai <125114735+KingHaHaHai@users.noreply.github.com>
2023-04-29 11:37:07 +0800

    Add files via upload

--- ttt.py
+++ ttt.py
@@ -2,6 +2,9 @@ import machine
 import time
 import dht
 import ssd1306
+import utime
+import network
+import urequests
 
 I2C = machine.SoftI2C(sda=machine.Pin(21),scl=machine.Pin(22))
 m水位 = machine.ADC(machine.Pin(32))
@@ -13,13 +16,36 @@ mpH = machine.ADC(machine.Pin(35))
 data = {}
 _a = 0
 
+# Wifi
+wifi_id = "snow777"
+wifi_pw = "snow123123"
+
+sta_if = network.WLAN(network.STA_IF)
+if sta_if.status() != 1010:
+    sta_if.active(True)
+    sta_if.connect(wifi_id,wifi_pw)
+    while not sta_if.isconnected():
+        pass
+print("wifi已连接")
+
+
 while 1:
-    if _a > 10:
+    try:
+        m温湿.measure()
+        print(1)
         break
-    vTime = time.time()
+    except: pass
+    finally: time.sleep(3)
+time.sleep(3)
+
+
+while 1:
     
+        
+    vTime = utime.localtime() # (YYYY,M,DD,...)
+    _time = "{}{:0>2d}{:0>2d}{:0>2d}{:0>2d}{:0>2d}".format(vTime[0],vTime[1],vTime[2],vTime[3],vTime[4],vTime[5])
     m温湿.measure()
-    print(vTime)
+    print(_time)
     mV水位 = m水位.read()
     print("mV水位",mV水位)
     mVCO2 = mCO2.read()
@@ -37,18 +63,22 @@ while 1:
     #t_data['湿'] = mV温湿_湿
     #t_data['pH'] = mVpH
     t_data = [mV水位,mVCO2,mV温湿_温,mV温湿_湿,mVpH]
-    data[str(vTime)] = t_data
+    # data[str(_time)] = t_data 
     
+    """
     with open("data.json", 'wb') as f:
         f.write(str(data).replace("'",'"').replace("},","},\n").encode('utf-8'))
+    """
+    
+    urequests.get(f'http://maker.ifttt.com/trigger/plant_data/with/key/evkQF4nTovqng89zeqQx2v5mgj66dfxPnarYWwPFqDB?value1={_time}&value2={str(t_data)[1:-1]}')
     
     m显示屏.text(str(mV温湿_温),0,0)
     m显示屏.text(str(mV温湿_湿),0,10)
     m显示屏.show()
+    del vTime,_time,mV水位,mVCO2,mV温湿_温,mV温湿_湿,mVpH,t_data
     time.sleep(3)
     # _a += 1
+    # 运行 11 次后出现神秘报错 OSError: 23
     
-    
-print(str(data).replace("'",'"').encode('utf-8'))
-# {"a": 123, }
-    
+print("完成")
+# print(str(data).replace("'",'"').encode('utf-8'))
